5d18a5cbeec2cdba628486c56b01d89c835d4cde
Paul Irish <paul.irish@gmail.com>
2016-05-24 23:46:08 -0700

    html building. still working.

--- static/app.js
+++ static/app.js
@@ -6,13 +6,12 @@ var bindFeatureDataList = function(features, required, onItem, sumCurrent) {
     $("#featureList").html("");
     $("#free").html("");
     for(var i in features) {
-    var supportedBy = BrowserStats.browsers.browsersByFeature(required.concat(i), ["y", "y x", "a", "a x"]);
-var sum = _.reduce(supportedBy, function(memo, num){ return memo + num.share; }, 0);
-    var difference = (sum / sumCurrent) * 100;
-    var actual = sumCurrent - sum;
+        var supportedBy = BrowserStats.browsers.browsersByFeature(required.concat(i), ["y", "y x", "a", "a x"]);
+        var sum = _.reduce(supportedBy, function(memo, num){ return memo + num.share; }, 0);
+        var difference = (sum / sumCurrent) * 100;
+        var actual = sumCurrent - sum;
 
-    $("#featureList").append("<option value='" + i  + "'>" +  difference.toFixed(2)  + "%</option>");
-    onItem({"id": i, "title": BrowserStats.browsers.getFeature(i).title, "difference": difference, "actual": sum});
+        buildAdditionalFeatures({"id": i, "title": BrowserStats.browsers.getFeature(i).title, "difference": difference, "actual": sum});
     }
 };
 
@@ -37,14 +36,10 @@ $(function() {
     var features = browsers.features;
     _.each(features, function(itm, idx) { itm.id = idx });
     var feats = _.sortBy(_.keys(features), function(itm) { return itm; });
+
+
     
-    $("#features")[0].innerHTML = feats.map(function(feat){
-        var feat = features[feat];
-        return "<li data-feature='" + feat.id + "'><label for='" + feat.id + "chk'>" + feat.title + "</label><span class=pctholder><span class=featpct></span></span>";
-    }).join("");
-    
-    
-    var updateShare = function(requiredFeatures) {
+    function updateShare(requiredFeatures) {
         if(!!requiredFeatures === false)  return;
         var total = BrowserStats.browsers.browsersByFeature([], ["y", "y x", "a", "a x"]);
         var supportedBy = BrowserStats.browsers.browsersByFeature(requiredFeatures, ["y", "y x", "a", "a x"]);
@@ -53,7 +48,7 @@ $(function() {
         var totalSum = _.reduce(total, function(memo, num){ return memo + num.share; }, 0);
         $("#share").css({"color": "hsla(" + Math.round((90 / 100) * (sum /totalSum * 100)) + ", 100%, 42%, 1)"  }).text((sum / totalSum * 100).toFixed(2));
 
-        bindFeatureDataList(features, requiredFeatures, buildAdditionalFeatures, sum)
+        bindFeatureDataList(features, requiredFeatures, undefined, sum)
         // Version numbers aren't that interesting here.
     //   drawTable("#totalShare",["name", "since", "share"], supportedBy);
     //   drawPie("#totalShareChart",["name", "since", "share"], supportedBy);
@@ -63,8 +58,18 @@ $(function() {
     //   drawPie("#mobileDesktopSplitChart",["name", "share"], mobileDesktopSplitData); 
     };
 
+    var categories = Object.keys(browsers.featureCats).sort();
+    var allHTML = categories.map(function(cat){
+        var feats = browsers.featureCats[cat];
+        var titleHTML = '</ul><h3>' + cat + '</h3><ul>';
+        var categoryHTML = feats.map(function(feat){
+            return "<li data-feature='" + feat.id + "'><label for='" + feat.id + "chk'>" + feat.title + "</label><span class=pctholder><span class=featpct></span></span>";
+        }).join("");
+        return titleHTML + categoryHTML;
 
-
+    })
+    $("#features")[0].innerHTML = allHTML;
+    
     var urlFeats = getFeatureArrayFromString(window.location.hash.substring(1));
     updateShare(urlFeats);
 
