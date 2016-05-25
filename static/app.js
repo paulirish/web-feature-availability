5cca4a4403a4d3331f55961874f3352234fe4e7a
Paul Irish <paul.irish@gmail.com>
2016-05-25 00:08:43 -0700

    it works with the big html dump. woo

--- static/app.js
+++ static/app.js
@@ -3,25 +3,39 @@ var bindFeatureDataList = function(features, required, onItem, sumCurrent) {
     var onItem = onItem || function() {};
     var supportedByCurrent = BrowserStats.browsers.browsersByFeature(required, ["y", "y x", "a", "a x"]);
     //var sumCurrent = _.reduce(supportedByCurrent, function(memo, num){ return memo + num.share; }, 0);
-    $("#featureList").html("");
-    $("#free").html("");
-    for(var i in features) {
-        var supportedBy = BrowserStats.browsers.browsersByFeature(required.concat(i), ["y", "y x", "a", "a x"]);
+    
+    return Object.keys(features).map(function(key){
+        var supportedBy = BrowserStats.browsers.browsersByFeature(required.concat(key), ["y", "y x", "a", "a x"]);
         var sum = _.reduce(supportedBy, function(memo, num){ return memo + num.share; }, 0);
         var difference = (sum / sumCurrent) * 100;
         var actual = sumCurrent - sum;
 
-        buildAdditionalFeatures({"id": i, "title": BrowserStats.browsers.getFeature(i).title, "difference": difference, "actual": sum});
-    }
+        return buildAdditionalFeatures({
+            "id": key, 
+            "title": BrowserStats.browsers.getFeature(key).title, 
+            "difference": difference, 
+            "actual": sum
+        });
+    })
 };
 
 var buildAdditionalFeatures = function(item) {
     var hue = Math.round(item.difference);
-    var elem = document.querySelector("li[data-feature='" + item.id  + "']");
-    var span = elem.querySelector('.featpct');
-    elem.querySelector('label').style["border-color"] = "hsla(" + hue + ", 100%, 42%, 1)";
+    var pct = item.difference.toFixed(0) + "%";
+    return {
+        id: item.id,
+        title: item.title,
+        difference: item.difference,
+        actual: item.actual, 
+        hue: hue, 
+        pct: pct };
+
+    var liElem = document.querySelector("li[data-feature='" + item.id  + "']");
+    var span = liElem.querySelector('.featpct');
+    var label = liElem.querySelector('label')
+    label.style["border-color"] = "hsla(" + hue + ", 100%, 42%, 1)";
     span.style['background-color'] = "hsla(" + hue + ", 100%, 42%, 1)";
-    span.style['width'] = (item.difference.toFixed(0) + "%")
+    span.style['width'] = pct;
     span.textContent = item.difference.toFixed(0) + "%";
 }
 
@@ -48,7 +62,7 @@ $(function() {
         var totalSum = _.reduce(total, function(memo, num){ return memo + num.share; }, 0);
         $("#share").css({"color": "hsla(" + Math.round((90 / 100) * (sum /totalSum * 100)) + ", 100%, 42%, 1)"  }).text((sum / totalSum * 100).toFixed(2));
 
-        bindFeatureDataList(features, requiredFeatures, undefined, sum)
+        return bindFeatureDataList(features, requiredFeatures, undefined, sum)
         // Version numbers aren't that interesting here.
     //   drawTable("#totalShare",["name", "since", "share"], supportedBy);
     //   drawPie("#totalShareChart",["name", "since", "share"], supportedBy);
@@ -58,31 +72,36 @@ $(function() {
     //   drawPie("#mobileDesktopSplitChart",["name", "share"], mobileDesktopSplitData); 
     };
 
+
+    var urlFeats = getFeatureArrayFromString(window.location.hash.substring(1));
+    var shareResults = updateShare(urlFeats);
+    shareResults = _.groupBy(shareResults, f => f.id );
+
     var categories = Object.keys(browsers.featureCats).sort();
     var allHTML = categories.map(function(cat){
         var feats = browsers.featureCats[cat];
         var titleHTML = '</ul><h3>' + cat + '</h3><ul>';
-        var categoryHTML = feats.map(function(feat){
-            return "<li data-feature='" + feat.id + "'><label for='" + feat.id + "chk'>" + feat.title + "</label><span class=pctholder><span class=featpct></span></span>";
-        }).join("");
-        return titleHTML + categoryHTML;
 
+        var categoryHTML = feats.map(function(feat){
+            var share = shareResults[feat.id][0];
+            var pct = share.pct;
+            var title = feat.title
+                .replace('CSS3 ','')
+//                 .replace('CSS ','')
+            var color = "hsla(" + share.hue + ", 100%, 42%, 1)";
+            return "" + 
+            "<li data-feature='" + feat.id + "'>" +
+            "<label style='border-color: " + color  + "' for='" + feat.id + "chk'>" + title + "</label>" + 
+            "<span class=pctholder>" + 
+                "<span class=featpct style='background-color:" + color + "; width: " + pct + "'>" + pct + "</span>" + 
+            "</span>";
+        }).join("");
+
+        return titleHTML + categoryHTML;
     })
     $("#features")[0].innerHTML = allHTML;
     
-    var urlFeats = getFeatureArrayFromString(window.location.hash.substring(1));
-    updateShare(urlFeats);
-
-    $(_.toArray(_.map(urlFeats, function(f) { return "input[data-feature='" + f + "']" })).join()).prop("checked", "checked"); 
     
-    $("#search").on('change', function() {
-        if(this.value === "") return;
-        var urlFeats = getFeatureArrayFromString(window.location.hash.substring(1));
-        urlFeats.push(this.value);
-        $(_.toArray(_.map(urlFeats, function(f) { return "input[data-feature='" + f + "']" })).join()).prop("checked", "checked"); 
-        window.location.hash = $("input:checked").map(function(val, i) { return $(i).data("feature"); }).toArray().join(",");
-        this.value = ""
-    });
     }); 
 });
 
