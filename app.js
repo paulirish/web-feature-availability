c327c7ad3fdfdc853df6cf19ccb39999e072823f
Paul Irish <commits@paul.irish>
2021-04-06 16:47:01 -0700

    format w/ prettier

--- app.js
+++ app.js
@@ -3,48 +3,50 @@ window.$ = document.querySelector.bind(document);
 window.$$ = document.querySelectorAll.bind(document);
 Node.prototype.on = window.on = function (name, fn) {
   this.addEventListener(name, fn);
-}
+};
 NodeList.prototype.__proto__ = Array.prototype;
 NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
   this.forEach(function (elem, i) {
     elem.on(name, fn);
   });
-}
+};
 
 // honestly i couldn't tell you how much of this code is from me over the past 5 years and how much is kinlan's fault (https://github.com/PaulKinlan/iwanttouse.com)
 // but i can tell you that the code isn't elegant
 // so, i'm sorry.
 
-
 //
 // app
 //
 
-var bindFeatureDataList = function(features, required, onItem, sumCurrent) {
-    return Object.keys(features).map(function(key){
-
-        return {
-            "id": key,
-            "title": BrowserStats.browsers.getFeature(key).title,
-        };
-    })
+var bindFeatureDataList = function (features, required, onItem, sumCurrent) {
+  return Object.keys(features).map(function (key) {
+    return {
+      id: key,
+      title: BrowserStats.browsers.getFeature(key).title,
+    };
+  });
 };
 
-document.on('DOMContentLoaded', function() {
-    var deviceType = "all";
+document.on('DOMContentLoaded', function () {
+  var deviceType = 'all';
 
-    BrowserStats.load(deviceType, function(browsers) {
+  BrowserStats.load(deviceType, function (browsers) {
     var features = browsers.features;
-    _.each(features, function(itm, idx) { itm.id = idx });
+    _.each(features, function (itm, idx) {
+      itm.id = idx;
+    });
 
     function updateShare(requiredFeatures) {
-        if(!!requiredFeatures === false)  return;
-        return bindFeatureDataList(features, requiredFeatures, undefined, 100)
-    };
+      if (!!requiredFeatures === false) return;
+      return bindFeatureDataList(features, requiredFeatures, undefined, 100);
+    }
 
     var urlFeats = getFeatureArrayFromString('');
     var shareResults = updateShare(urlFeats);
-    shareResults = _.groupBy(shareResults, function(f){ return f.id });
+    shareResults = _.groupBy(shareResults, function (f) {
+      return f.id;
+    });
 
     // merge a few categories together
     browsers.featureCats.CSS = [...browsers.featureCats.CSS, ...browsers.featureCats.CSS3];
@@ -57,49 +59,58 @@ document.on('DOMContentLoaded', function() {
     const hasParam99 = new URL(location.href).searchParams.has('99');
 
     var categories = Object.keys(browsers.featureCats).sort();
-    var allHTML = categories.map(function(cat){
-        const feats = browsers.featureCats[cat];
+    var allHTML = categories.map(function (cat) {
+      const feats = browsers.featureCats[cat];
 
-        const renamedCat = ({'JS API': 'Browser APIs'})[cat] || cat;
-        const titleHTML = `</ul><h3>${renamedCat}</h3><ul>`;
+      const renamedCat = {'JS API': 'Browser APIs'}[cat] || cat;
+      const titleHTML = `</ul><h3>${renamedCat}</h3><ul>`;
 
-        // smush those results onto the objects
-        feats.forEach(function(feat){ feat.share = shareResults[feat.id][0]; });
+      // smush those results onto the objects
+      feats.forEach(function (feat) {
+        feat.share = shareResults[feat.id][0];
+      });
 
-        const categoryHTML = feats
-        .map(feat => { feat.totalSupport = feat.usage_perc_a + feat.usage_perc_y; return feat; })
-        .filter(feat => {
-            // Previously i filtered for features with a chrome_id.. (as a proxy for recent stuff?) but it misses out on some stuff...
-            // now we filter for obviously supported stuff (but allowlist 'JS' stuff because _developit said so.)
-            if (hasParamAll) return true;
-            if (hasParam99) return feat.totalSupport >=  98 && cat !== 'JS';
-
-            return feat.totalSupport < 98 || cat === 'JS';
+      const categoryHTML = feats
+        .map(feat => {
+          feat.totalSupport = feat.usage_perc_a + feat.usage_perc_y;
+          return feat;
         })
-        .sort(function(a, b) { return b.totalSupport - a.totalSupport})
-        .map(function(feat){
-            var adjustedHue = adjustHue(feat.totalSupport);
-            var color = `hsla(${adjustedHue}, 100%, 42%, 1)`;
+        .filter(feat => {
+          // Previously i filtered for features with a chrome_id.. (as a proxy for recent stuff?) but it misses out on some stuff...
+          // now we filter for obviously supported stuff (but allowlist 'JS' stuff because _developit said so.)
+          if (hasParamAll) return true;
+          if (hasParam99) return feat.totalSupport >= 98 && cat !== 'JS';
 
-            var partialColor = `hsla(${adjustedHue}, 90%, 39.6%, 1)`;
-            const fullSupportPct = feat.usage_perc_y;
-            const partialSupportPct = feat.usage_perc_a;
+          return feat.totalSupport < 98 || cat === 'JS';
+        })
+        .sort(function (a, b) {
+          return b.totalSupport - a.totalSupport;
+        })
+        .map(function (feat) {
+          var adjustedHue = adjustHue(feat.totalSupport);
+          var color = `hsla(${adjustedHue}, 100%, 42%, 1)`;
 
-            var pct = `${escape(feat.totalSupport.toLocaleString(undefined, {maximumFractionDigits: 1}))}%`;
-            var title = feat.title
-                .replace(`CSS3 `,``)
-                .replace(`CSS `,``)
-                .replace(`(rounded corners)`,``);
+          var partialColor = `hsla(${adjustedHue}, 90%, 39.6%, 1)`;
+          const fullSupportPct = feat.usage_perc_y;
+          const partialSupportPct = feat.usage_perc_a;
 
-            totalFeatures++;
-            allfeats.push(feat);
+          var pct = `${escape(
+            feat.totalSupport.toLocaleString(undefined, {maximumFractionDigits: 1})
+          )}%`;
+          var title = feat.title
+            .replace(`CSS3 `, ``)
+            .replace(`CSS `, ``)
+            .replace(`(rounded corners)`, ``);
 
-            return `
+          totalFeatures++;
+          allfeats.push(feat);
+
+          return `
             <li data-feature='${feat.id}'>
-                <label style='border-color: ${color }' title='${title} — ${escape(feat.description)}'>
+                <label style='border-color: ${color}' title='${title} — ${escape(feat.description)}'>
                     <a href=http://caniuse.com/#feat=${feat.id}>${title}</a>
                 </label>
-                <span class='pctholder ${(feat.totalSupport < 30) ? "lessThan30" : ""}'>
+                <span class='pctholder ${feat.totalSupport < 30 ? 'lessThan30' : ''}'>
                     <em>${pct}</em>
                     <span class=featpct
                         style='background-color:${color}; width: ${fullSupportPct}%'>
@@ -114,55 +125,58 @@ document.on('DOMContentLoaded', function() {
                            width: ${partialSupportPct}%'>
                     </span>
                 </span>`;
-        }).join("");
+        })
+        .join('');
 
-        // dont make a heading for an empty section (CSS2)
-        if (!categoryHTML) return '';
+      // dont make a heading for an empty section (CSS2)
+      if (!categoryHTML) return '';
 
-        return titleHTML + categoryHTML;
-    })
-    $("#features").innerHTML = allHTML.join('');
+      return titleHTML + categoryHTML;
+    });
+    $('#features').innerHTML = allHTML.join('');
 
     $('#search').placeholder = `Filter across ${totalFeatures.toLocaleString()} features…`;
 
     setupSearch();
 
     updateDates(browsers);
-
-    });
+  });
 });
 
 window.allfeats = [];
 
 function escape(str) {
-    return str.replace(/'/g, "")
+  return str.replace(/'/g, '');
 }
 
-var getFeatureArrayFromString = function(str) {
-    var feats = str.split(",");
-    if(feats.length == 1 && feats[0] === "")  return [];
-    return feats
+var getFeatureArrayFromString = function (str) {
+  var feats = str.split(',');
+  if (feats.length == 1 && feats[0] === '') return [];
+  return feats;
 };
 
 function updateDates(browsers) {
-    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-    $('#caniuse-date').textContent = new Date(browsers.updated * 1000).toLocaleDateString(undefined, dateOptions);
+  const dateOptions = {year: 'numeric', month: 'long', day: 'numeric'};
+  $('#caniuse-date').textContent = new Date(browsers.updated * 1000).toLocaleDateString(
+    undefined,
+    dateOptions
+  );
 }
 
 function setupSearch() {
-    var searchInput = document.getElementById('search');
+  var searchInput = document.getElementById('search');
 
-    searchInput.addEventListener('input', function() {
-        document.body.classList.toggle('is-searching', searchInput.value);
-    });
+  searchInput.addEventListener('input', function () {
+    document.body.classList.toggle('is-searching', searchInput.value);
+  });
 
-    new Jets({
-        searchTag: '#search',
-        contentTag: '#features > ul'
-    });
+  new Jets({
+    searchTag: '#search',
+    contentTag: '#features > ul',
+  });
 }
 
 function adjustHue(hue) {
-    // what math this is i have NO idea
-  return Math.pow(hue,3)/10000;
+  // what math this is i have NO idea
+  return Math.pow(hue, 3) / 10000;
 }
--- browser-stats.js
+++ browser-stats.js
@@ -1,48 +1,54 @@
-var BrowserStats = (function() {
-  var Browser = function(key, agent) {
+var BrowserStats = (function () {
+  var Browser = function (key, agent) {
     var _agent = agent;
     var _Share;
 
-    Object.defineProperty(this, "browser", {
-      "get": function() {
+    Object.defineProperty(this, 'browser', {
+      get: function () {
         return _agent.browser;
-      }
+      },
     });
 
-    Object.defineProperty(this, "share", {
-      "get": function() {
+    Object.defineProperty(this, 'share', {
+      get: function () {
         return _agent.usage_global;
-      }
+      },
     });
 
-    Object.defineProperty(this, "type", {
-      "get": function() {
+    Object.defineProperty(this, 'type', {
+      get: function () {
         return _agent.type;
-      }
+      },
     });
 
-    Object.defineProperty(this, "versionKeys", {
-      "get": function() {
+    Object.defineProperty(this, 'versionKeys', {
+      get: function () {
         var versions = [];
-        for(var v in _agent.versions) {
-          if(!!_agent.versions[v]) {
-            versions.push(key + "+" + _agent.versions[v]);
+        for (var v in _agent.versions) {
+          if (!!_agent.versions[v]) {
+            versions.push(key + '+' + _agent.versions[v]);
           }
-
         }
         return versions;
-      }
+      },
     });
 
-    this.getVersionShare = function(version) {
+    this.getVersionShare = function (version) {
       return _agent.usage_global[version] || 0;
     };
 
-    Object.defineProperty(this, "totalShare", {
-      "get": function() {
-        if(!!_Share == false) _Share = _.reduce(_agent.usage_global, function(memo, num){ return memo + num }, 0);
+    Object.defineProperty(this, 'totalShare', {
+      get: function () {
+        if (!!_Share == false)
+          _Share = _.reduce(
+            _agent.usage_global,
+            function (memo, num) {
+              return memo + num;
+            },
+            0
+          );
         return _Share;
-      }
+      },
     });
   };
 
@@ -50,67 +56,70 @@ var BrowserStats = (function() {
     var _agents = {};
     var _features = {};
 
-    this.__defineGetter__("features", function() {
+    this.__defineGetter__('features', function () {
       return _features;
     });
 
-    this.addBrowser = function(a, agent) {
+    this.addBrowser = function (a, agent) {
       _agents[a] = new Browser(a, agent);
     };
 
-
-    this._getBrowser = function(key) {
-      var ua = key.split("+");
+    this._getBrowser = function (key) {
+      var ua = key.split('+');
       var agent = _agents[ua[0]];
-      return { "key": key, "version": ua[1], "type": agent.type, "name": agent.browser , "browserShare": agent.getVersionShare(ua[1]) };
+      return {
+        key: key,
+        version: ua[1],
+        type: agent.type,
+        name: agent.browser,
+        browserShare: agent.getVersionShare(ua[1]),
+      };
     };
     this.browserCache = new Map();
-    this.getBrowser = function(key) {
+    this.getBrowser = function (key) {
       var res = this.browserCache.get(key);
-      if (!res){
+      if (!res) {
         res = this._getBrowser(key);
         this.browserCache.set(key, res);
       }
       return res;
-    }
+    };
 
-    this.addFeature = function(feature, versions) {
+    this.addFeature = function (feature, versions) {
       _features[feature] = versions;
     };
 
     this.cleanCache = new Map();
     this.stripRegex = / #\d+/g;
     // strip notes_by_num annotation.. https://github.com/Fyrd/caniuse/blob/master/CONTRIBUTING.md
-    this.cleanAnnotations = function(stat){
+    this.cleanAnnotations = function (stat) {
       var res = this.cleanCache.get(stat);
-      if (!res)
-        res = stat.replace(this.stripRegex,'');
-        this.cleanCache.set(stat, res);
+      if (!res) res = stat.replace(this.stripRegex, '');
+      this.cleanCache.set(stat, res);
       return res;
-    }
+    };
 
-    this.getByFeature = function(features, states) {
+    this.getByFeature = function (features, states) {
       var output = [];
-      if(!!features == false || features.length === 0) {
+      if (!!features == false || features.length === 0) {
         // get every single browser and version
         var agents = [];
-        for(var a in _agents) {
+        for (var a in _agents) {
           agents = agents.concat(_agents[a].versionKeys);
         }
         output.push(agents);
-      }
-      else {
-        for(var f = 0; f < features.length; f++) {
+      } else {
+        for (var f = 0; f < features.length; f++) {
           output.push([]);
           var feat = features[f];
           var feature = _features[feat];
-          for(var b in feature.stats) {
-            for(var v in feature.stats[b]) {
+          for (var b in feature.stats) {
+            for (var v in feature.stats[b]) {
               var present = feature.stats[b][v];
               present = this.cleanAnnotations(present);
 
-              if(states.indexOf(present) !== -1) {
-                var browserStr = b + "+" + v;
+              if (states.indexOf(present) !== -1) {
+                var browserStr = b + '+' + v;
                 output[f].push(this.getBrowser(browserStr));
               }
             }
@@ -121,100 +130,111 @@ var BrowserStats = (function() {
       return output[0];
     };
 
-    this.browsersByFeature = function(features, states) {
-      return featuresByProperty.call(this, features, states, "name");
+    this.browsersByFeature = function (features, states) {
+      return featuresByProperty.call(this, features, states, 'name');
     };
 
-    this.typesByFeature =  function(features, states) {
-      return featuresByProperty.call(this, features, states, "type");
+    this.typesByFeature = function (features, states) {
+      return featuresByProperty.call(this, features, states, 'type');
     };
 
-    var featuresByProperty = function(features, states, property) {
+    var featuresByProperty = function (features, states, property) {
       var supportedBy = this.getByFeature(features, states);
-      var versionsByBrowser = _.groupBy(supportedBy, function(i) { return i[property] });
-      return Object.keys(versionsByBrowser).map(function(i) {
-        i = versionsByBrowser[i]
-        var share = i.reduce(function(memo, r){ return memo + r.browserShare}, 0 );
+      var versionsByBrowser = _.groupBy(supportedBy, function (i) {
+        return i[property];
+      });
+      return Object.keys(versionsByBrowser).map(function (i) {
+        i = versionsByBrowser[i];
+        var share = i.reduce(function (memo, r) {
+          return memo + r.browserShare;
+        }, 0);
         return {
-          "name": i[0][property],
-          "share": share
-        }
-    });
+          name: i[0][property],
+          share: share,
+        };
+      });
     };
 
-    this.getFeature = function(featureName) {
+    this.getFeature = function (featureName) {
       return _features[featureName];
-
     };
   };
 
-  var load = function(type, callback) {
-    callback = callback || function() {};
-    fetch("data.json").then(function(d){ return d.json()}).then(function(data) { parse(type, data, callback); });
+  var load = function (type, callback) {
+    callback = callback || function () {};
+    fetch('data.json')
+      .then(function (d) {
+        return d.json();
+      })
+      .then(function (data) {
+        parse(type, data, callback);
+      });
   };
 
   var browsers = new Browsers();
 
-  var parse = function(type, data, callback) {
-  var validAgents = {};
-    for(var a in data.agents) {
-    if(type == "all" || type == data.agents[a].type) {
+  var parse = function (type, data, callback) {
+    var validAgents = {};
+    for (var a in data.agents) {
+      if (type == 'all' || type == data.agents[a].type) {
         browsers.addBrowser(a, data.agents[a]);
         validAgents[a] = true;
       }
     }
 
-    for(var i in data.data) {
-    // Remove agents that are not part of the viewed set.
-    var feature = data.data[i]
-    for(var a in feature.stats) {
-    if(!!validAgents[a] == false) {
-      feature.stats[a] = undefined
-    }
-    }
+    for (var i in data.data) {
+      // Remove agents that are not part of the viewed set.
+      var feature = data.data[i];
+      for (var a in feature.stats) {
+        if (!!validAgents[a] == false) {
+          feature.stats[a] = undefined;
+        }
+      }
       browsers.addFeature(i, feature);
     }
-    browsers.featureCats = _.groupBy(browsers.features, function(t) { return t.categories[0]});
+    browsers.featureCats = _.groupBy(browsers.features, function (t) {
+      return t.categories[0];
+    });
 
     browsers.updated = data.updated;
 
     callback(browsers);
   };
 
-  var intersect = function() {
+  var intersect = function () {
     var itemCount = arguments.length;
     var items = {};
 
-    for(var i in arguments) {
+    for (var i in arguments) {
       // invert the list.
       var arg = arguments[i];
-      for(var a in arg) {
-        if(!!items[a] == false) items[a] = 0;
+      for (var a in arg) {
+        if (!!items[a] == false) items[a] = 0;
         items[a]++;
       }
     }
 
     var output = [];
 
-    for(var i in items) {
-      if(items[i] == itemCount) output.push(i);
+    for (var i in items) {
+      if (items[i] == itemCount) output.push(i);
     }
 
     return output;
   };
 
   var returnObject = {
-  load: load,
-  browsers: function(type) {
-    if(!!type == false) return browsers;
-    else return
-  }
+    load: load,
+    browsers: function (type) {
+      if (!!type == false) return browsers;
+      else return;
+    },
   };
 
-  Object.defineProperty(returnObject, "browsers", {
-    "get": function() {
+  Object.defineProperty(returnObject, 'browsers', {
+    get: function () {
       return browsers;
-    }
+    },
   });
 
   return returnObject;
